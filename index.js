392910db07ec82109509375bc5b870887676320e
Tommaso Sebastianelli <tommaso-sebastianelli@users.noreply.github.com>
2023-03-14 21:08:26 +0100

    fix: add useNativeDriver prop to Animated

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
-import React from 'react'
-import { View, StyleSheet, Animated, Easing } from 'react-native'
+import React from "react";
+import { Animated, StyleSheet, View } from "react-native";
 
-const SIZE = 7
-const MARGIN = 5
-const BG = 'rgb(172, 172, 172)'
-const ACTIVE_BG = '#808184'
-const dots = [1, 2, 3]
-const INTERVAL = 300
-const ANIMATION_DURATION = 400
-const ANIMATION_SCALE = 1.4
+const SIZE = 7;
+const MARGIN = 5;
+const BG = "rgb(172, 172, 172)";
+const ACTIVE_BG = "#808184";
+const dots = [1, 2, 3];
+const INTERVAL = 300;
+const ANIMATION_DURATION = 400;
+const ANIMATION_SCALE = 1.4;
 export default class ThreeDotsLoader extends React.Component {
   state = {
-    active: 1
-  }
+    active: 1,
+  };
 
-  componentDidMount(){
+  componentDidMount() {
     this.interval = setInterval(() => {
-      const active = this.state.active
-      this.setState({ active: active > 2 ? 1 : active + 1})
+      const active = this.state.active;
+      this.setState({ active: active > 2 ? 1 : active + 1 });
     }, INTERVAL);
   }
 
-  componentWillUnmount(){
-    clearInterval(this.interval)
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 
   render() {
-  const active = this.state.active
-   return (
-     <View style={styles.main}>
-       {dots.map(i => <Dot {...this.props} active={i === active}/>)}
-     </View>
-   )
+    const active = this.state.active;
+    return (
+      <View style={styles.main}>
+        {dots.map((i) => (
+          <Dot {...this.props} active={i === active} />
+        ))}
+      </View>
+    );
   }
 }
 
@@ -43,65 +45,62 @@ class Dot extends React.Component {
     dotMargin: MARGIN,
     animationDuration: ANIMATION_DURATION,
     animationScale: ANIMATION_SCALE,
-  }
+  };
 
   constructor(props) {
-    super(props)
-    this.scale = new Animated.Value(1)
+    super(props);
+    this.scale = new Animated.Value(1);
   }
 
-  componentDidMount(){
-    if (this.props.active) this.scaleUp()
+  componentDidMount() {
+    if (this.props.active) this.scaleUp();
   }
 
-  componentDidUpdate(prevProps){
-    if (prevProps.active && !this.props.active){
-      this.scaleDown()
+  componentDidUpdate(prevProps) {
+    if (prevProps.active && !this.props.active) {
+      this.scaleDown();
     }
-    if (!prevProps.active && this.props.active){
-      this.scaleUp()
+    if (!prevProps.active && this.props.active) {
+      this.scaleUp();
     }
   }
 
   scaleDown = () => {
-    Animated.timing(
-      this.scale,
-      {
-        toValue: 1,
-        duration: this.props.animationDuration,
-      }
-    ).start()
-  }
+    Animated.timing(this.scale, {
+      toValue: 1,
+      duration: this.props.animationDuration,
+      useNativeDriver: false,
+    }).start();
+  };
 
   scaleUp = () => {
-    Animated.timing(
-      this.scale,
-      {
-        toValue: this.props.animationScale,
-        duration: this.props.animationDuration,
-      }
-    ).start()
-  }
+    Animated.timing(this.scale, {
+      toValue: this.props.animationScale,
+      duration: this.props.animationDuration,
+      useNativeDriver: false,
+    }).start();
+  };
 
-  render(){
-    const { active, size, background, activeBackground, dotMargin } = this.props
+  render() {
+    const { active, size, background, activeBackground, dotMargin } =
+      this.props;
     const style = {
-      height: size, 
+      height: size,
       width: size,
       borderRadius: size / 2,
       marginHorizontal: dotMargin,
-      backgroundColor: active ? activeBackground : background
-    }
+      backgroundColor: active ? activeBackground : background,
+    };
     return (
-      <Animated.View style={[style, {transform: [{ scale: this.scale }]}]}/>
-    )
+      <Animated.View style={[style, { transform: [{ scale: this.scale }] }]} />
+    );
   }
 }
 
 const styles = StyleSheet.create({
   main: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
-})
+});
